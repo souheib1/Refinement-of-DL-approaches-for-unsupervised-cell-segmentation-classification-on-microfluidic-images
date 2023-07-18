54d434bc67fc16fca1e71bdb94d5095b8af0ffc0
Souheib Ben Mabrouk <souheib.benmabrouk@supcom.tn>
2023-07-18 14:54:16 +0200

    added some details

--- preprocess/test_preprocessing.py
+++ preprocess/test_preprocessing.py
@@ -6,17 +6,16 @@ from image_utils import load_images_from_path,save_preprocessed_images
 import os
 
 
-# Load images
 
 PATH_DATA = "../Data/"
 data_folders = os.listdir(PATH_DATA)
 
 for folder in data_folders: 
     PATH = os.path.join(PATH_DATA, folder, '*.tif')
-    image_paths, images = load_images_from_path(PATH)
+    image_paths, images = load_images_from_path(PATH,channel=1)
     #find rotation to align the image vertically
     teta = find_rotation(images[0])
     enhanced_images = [preprocess(image,teta) for image in images]
     experiment = str(PATH.split('/')[2])[:-12]
     print(experiment)
-    save_preprocessed_images(enhanced_images, output_path= './' + experiment + '/preprocessed_images/')
+    save_preprocessed_images(enhanced_images, output_path= './' + experiment)
