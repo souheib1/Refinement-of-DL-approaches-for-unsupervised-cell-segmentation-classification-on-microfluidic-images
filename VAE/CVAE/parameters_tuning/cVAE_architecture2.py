c756dec18f510868189f5a9b660da70a798ce2ac
Souheib Ben Mabrouk <73786465+souheib1@users.noreply.github.com>
2023-09-09 13:54:08 +0200

    update clustering notebook for architecture2

--- VAE/CVAE/parameters_tuning/cVAE_architecture2.py
+++ VAE/CVAE/parameters_tuning/cVAE_architecture2.py
@@ -1,3 +1,148 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+#!/usr/bin/env python
+# coding: utf-8
+# @author Souheib Ben Mabrouk
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from torch.autograd import Variable
+import torch.utils.data as data_utils 
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+batch_size = 128
+
+class cVAE2(nn.Module):
+    
+    def __init__(self, num_labels=10, n_channels=1, convDim1=64, convDim2=128, latent_dim=16, input_size=28, kernel_size=4, beta=1, alpha = 8):
+        super().__init__()
+
+        self.kernel_size = kernel_size
+        self.convDim1 = convDim1
+        self.convDim2 = convDim2
+        self.latent_dim = latent_dim
+        self.input_size = input_size
+        self.label = nn.Embedding(num_labels, latent_dim)
+        self.linear1 = nn.Linear(latent_dim,latent_dim - latent_dim // alpha )
+        self.linear2 = nn.Linear(latent_dim,latent_dim // alpha)
+        self.contact_activation = nn.Sigmoid()        
+        self.beta = beta
+        # Calculate the number of output features after encoder convolutions
+    
+        self.encoder_output_size = self.calculate_encoder_output_size()
+        self.decoder_output_size = convDim2 * (self.input_size // 4) * (self.input_size // 4)
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(n_channels, convDim1, kernel_size, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(convDim1, convDim2, kernel_size, stride=2, padding=1),
+            nn.BatchNorm2d(convDim2),
+            nn.ReLU()
+        )
+
+        self.flatten_z_mean = nn.Linear(self.encoder_output_size, out_features=latent_dim)
+        self.flatten_z_log_var = nn.Linear(self.encoder_output_size, out_features=latent_dim)
+        self.softplus = nn.Softplus()
+
+        self.decode_linear = nn.Linear(latent_dim, self.encoder_output_size)
+        self.decode_2 = nn.ConvTranspose2d(in_channels=convDim2, out_channels=convDim1,
+                                           kernel_size=kernel_size, stride=2, padding=1)
+        self.decode_1 = nn.ConvTranspose2d(in_channels=convDim1, out_channels=n_channels,
+                                           kernel_size=kernel_size, stride=2, padding=1)
+
+
+
+    def calculate_encoder_output_size(self):
+        # Calculate the output size after encoder convolutions
+        def conv_output_size(input_size, kernel_size=self.kernel_size, stride=2, padding=1):
+            return (input_size - kernel_size + 2 * padding) // stride + 1
+        
+        conv1_output_size = conv_output_size(self.input_size, kernel_size=4, stride=2, padding=1)
+        conv2_output_size = conv_output_size(conv1_output_size, kernel_size=4, stride=2, padding=1)
+        return self.convDim2 * conv2_output_size * conv2_output_size
+
+
+
+    def sampling(self,z_mean, z_log_var):
+        batch = z_mean.shape[0]
+        dim = z_mean.shape[1]
+        # generates random samples from a standard normal distribution
+        epsilon = torch.randn(batch, dim, device=z_mean.device)
+        #The reparameterization trick
+        return z_mean + torch.exp(0.5 * z_log_var) * epsilon
+        
+        
+    def encode(self, x,y):
+        y = self.label(y)
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        z_mean  = self.flatten_z_mean(x) 
+        z_log_var = self.softplus(self.flatten_z_log_var(x))
+        x = self.sampling(z_mean, z_log_var)
+        x = self.linear1(x)
+        y = self.linear2(y)
+        z = self.contact_activation(torch.cat((x, y), dim = 1))
+        z = z.view(z.size(0), -1)
+        return z
+    
+        
+    def decode(self, z):
+        x = self.decode_linear(z)
+        x = x.view(x.size(0), self.convDim2, self.input_size // 4, self.input_size // 4)
+        x = F.relu(self.decode_2(x))
+        reconstruction = F.sigmoid(self.decode_1(x))
+        return reconstruction
+    
+    
+    def forward(self, x, y):
+        y = self.label(y)
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        z_mean = self.flatten_z_mean(x)
+        z_log_var = self.softplus(self.flatten_z_log_var(x))
+        x = self.sampling(z_mean, z_log_var)
+        x = self.linear1(x)
+        y = self.linear2(y)
+        z = self.contact_activation(torch.cat((x, y), dim = 1))
+        z = z.view(z.size(0), -1)
+        x = self.decode_linear(z)
+        x = x.view(x.size(0), self.convDim2, self.input_size // 4, self.input_size // 4)
+        x = F.relu(self.decode_2(x))
+        reconstruction = F.sigmoid(self.decode_1(x))          
+        return z_mean, z_log_var, reconstruction
+    
+    
+def KL_divergence(z_mean, z_log_var):
+    #z_log_var = torch.clamp(loss, max=10)
+    loss = -0.5 * torch.mean(1 + z_log_var - z_mean.pow(2) - z_log_var.exp())
+    loss = F.softplus(loss)
+    clipped_loss = loss
+    # clipped_loss = torch.clamp(loss, max=10)
+    return clipped_loss
+
+
+def log_likelihood(x, reconstruction):
+    loss = nn.BCELoss(reduction='mean')
+    return loss(reconstruction, x)
+
+def mse(x, reconstruction):
+    loss = nn.MSELoss(reduction='mean')
+    return loss(reconstruction, x)
+
+
+def loss_vae(beta, original, z_mean, z_log_var, reconstructed):
+    total_loss = log_likelihood(original,reconstructed) + beta * KL_divergence(z_mean,z_log_var)
+    # print("BCE = ",log_likelihood(original,reconstructed))
+    # print("KL = ",KL_divergence(z_mean,z_log_var))
+    return total_loss
+=======
 #!/usr/bin/env python
 # coding: utf-8
 # @author Souheib Ben Mabrouk
@@ -140,3 +285,148 @@ def loss_vae(beta, original, z_mean, z_log_var, reconstructed):
     # print("BCE = ",log_likelihood(original,reconstructed))
     # print("KL = ",KL_divergence(z_mean,z_log_var))
     return total_loss
+>>>>>>> 61b1a12eda44206a83eec5925a04cbe96d1a7ec3
+=======
+#!/usr/bin/env python
+# coding: utf-8
+# @author Souheib Ben Mabrouk
+
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from torch.autograd import Variable
+import torch.utils.data as data_utils 
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+batch_size = 128
+
+class cVAE2(nn.Module):
+    
+    def __init__(self, num_labels=10, n_channels=1, convDim1=64, convDim2=128, latent_dim=16, input_size=28, kernel_size=4, beta=1):
+        super().__init__()
+
+        self.kernel_size = kernel_size
+        self.convDim1 = convDim1
+        self.convDim2 = convDim2
+        self.latent_dim = latent_dim
+        self.input_size = input_size
+        self.label = nn.Embedding(num_labels, latent_dim)
+        self.linear1 = nn.Linear(latent_dim,latent_dim - latent_dim // 8 )
+        self.linear2 = nn.Linear(latent_dim,latent_dim // 8)
+        self.contact_activation = nn.Sigmoid()        
+        self.beta = beta
+        # Calculate the number of output features after encoder convolutions
+    
+        self.encoder_output_size = self.calculate_encoder_output_size()
+        self.decoder_output_size = convDim2 * (self.input_size // 4) * (self.input_size // 4)
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(n_channels, convDim1, kernel_size, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(convDim1, convDim2, kernel_size, stride=2, padding=1),
+            nn.BatchNorm2d(convDim2),
+            nn.ReLU()
+        )
+
+        self.flatten_z_mean = nn.Linear(self.encoder_output_size, out_features=latent_dim)
+        self.flatten_z_log_var = nn.Linear(self.encoder_output_size, out_features=latent_dim)
+        self.softplus = nn.Softplus()
+
+        self.decode_linear = nn.Linear(latent_dim, self.encoder_output_size)
+        self.decode_2 = nn.ConvTranspose2d(in_channels=convDim2, out_channels=convDim1,
+                                           kernel_size=kernel_size, stride=2, padding=1)
+        self.decode_1 = nn.ConvTranspose2d(in_channels=convDim1, out_channels=n_channels,
+                                           kernel_size=kernel_size, stride=2, padding=1)
+
+
+
+    def calculate_encoder_output_size(self):
+        # Calculate the output size after encoder convolutions
+        def conv_output_size(input_size, kernel_size=self.kernel_size, stride=2, padding=1):
+            return (input_size - kernel_size + 2 * padding) // stride + 1
+        
+        conv1_output_size = conv_output_size(self.input_size, kernel_size=4, stride=2, padding=1)
+        conv2_output_size = conv_output_size(conv1_output_size, kernel_size=4, stride=2, padding=1)
+        return self.convDim2 * conv2_output_size * conv2_output_size
+
+
+
+    def sampling(self,z_mean, z_log_var):
+        batch = z_mean.shape[0]
+        dim = z_mean.shape[1]
+        # generates random samples from a standard normal distribution
+        epsilon = torch.randn(batch, dim, device=z_mean.device)
+        #The reparameterization trick
+        return z_mean + torch.exp(0.5 * z_log_var) * epsilon
+        
+        
+    def encode(self, x,y):
+        y = self.label(y)
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        z_mean  = self.flatten_z_mean(x) 
+        z_log_var = self.softplus(self.flatten_z_log_var(x))
+        x = self.sampling(z_mean, z_log_var)
+        x = self.linear1(x)
+        y = self.linear2(y)
+        z = self.contact_activation(torch.cat((x, y), dim = 1))
+        z = z.view(z.size(0), -1)
+        return z
+    
+        
+    def decode(self, z):
+        x = self.decode_linear(z)
+        x = x.view(x.size(0), self.convDim2, self.input_size // 4, self.input_size // 4)
+        x = F.relu(self.decode_2(x))
+        reconstruction = F.sigmoid(self.decode_1(x))
+        return reconstruction
+    
+    
+    def forward(self, x, y):
+        y = self.label(y)
+        x = self.encoder(x)
+        x = x.view(x.size(0), -1)
+        z_mean = self.flatten_z_mean(x)
+        z_log_var = self.softplus(self.flatten_z_log_var(x))
+        x = self.sampling(z_mean, z_log_var)
+        x = self.linear1(x)
+        y = self.linear2(y)
+        z = self.contact_activation(torch.cat((x, y), dim = 1))
+        z = z.view(z.size(0), -1)
+        x = self.decode_linear(z)
+        x = x.view(x.size(0), self.convDim2, self.input_size // 4, self.input_size // 4)
+        x = F.relu(self.decode_2(x))
+        reconstruction = F.sigmoid(self.decode_1(x))          
+        return z_mean, z_log_var, reconstruction
+    
+    
+def KL_divergence(z_mean, z_log_var):
+    #z_log_var = torch.clamp(loss, max=10)
+    loss = -0.5 * torch.mean(1 + z_log_var - z_mean.pow(2) - z_log_var.exp())
+    loss = F.softplus(loss)
+    clipped_loss = loss
+    # clipped_loss = torch.clamp(loss, max=10)
+    return clipped_loss
+
+
+def log_likelihood(x, reconstruction):
+    loss = nn.BCELoss(reduction='mean')
+    return loss(reconstruction, x)
+
+def mse(x, reconstruction):
+    loss = nn.MSELoss(reduction='mean')
+    return loss(reconstruction, x)
+
+
+def loss_vae(beta, original, z_mean, z_log_var, reconstructed):
+    total_loss = log_likelihood(original,reconstructed) + beta * KL_divergence(z_mean,z_log_var)
+    # print("BCE = ",log_likelihood(original,reconstructed))
+    # print("KL = ",KL_divergence(z_mean,z_log_var))
+    return total_loss
+>>>>>>> 61b1a12eda44206a83eec5925a04cbe96d1a7ec3
--- VAE/CVAE/parameters_tuning/cVAE_train_utils2.py
+++ VAE/CVAE/parameters_tuning/cVAE_train_utils2.py
@@ -1,3 +1,92 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+#!/usr/bin/env python
+# coding: utf-8
+# @author Souheib Ben Mabrouk
+
+
+import numpy as np 
+import torch
+import matplotlib.pyplot as plt
+from cVAE_architecture2 import loss_vae
+
+device='cuda:0' if torch.cuda.is_available() else 'cpu'
+clip_value = 1.0 
+batch_size = 128
+
+def clip_grads(model, clip_value):
+    for param in model.parameters():
+        if param.grad is not None:
+            param.grad.data = torch.clamp(param.grad.data, -clip_value, clip_value)
+
+def train_epoch(model, beta, criterion, optimizer, data_loader):
+    train_loss_per_epoch = []
+    model.train()
+    for x_batch, y in data_loader:
+        y = y.to(device)
+        x_batch = x_batch.to(device)    
+        z_mean, z_log_var, reconstruction = model(x_batch, y)
+        loss = criterion(beta, x_batch.to(device).float(), z_mean, z_log_var, reconstruction) 
+        optimizer.zero_grad()
+        loss.backward()
+        #clip_grads(model, 1.0)
+        optimizer.step()
+        train_loss_per_epoch.append(loss.item())
+    return np.mean(train_loss_per_epoch), z_mean, z_log_var, reconstruction
+
+
+def eval_epoch(model, beta, criterion, optimizer, data_loader):
+    val_loss_per_epoch = []
+    model.eval()
+    with torch.no_grad():
+        for x_val, y in data_loader:
+            y = y.to(device)
+            x_val = x_val.to(device)
+            z_mean, z_log_var, reconstruction = model(x_val, y)
+            loss = criterion(beta, x_val.to(device).float(), z_mean, z_log_var, reconstruction) 
+            val_loss_per_epoch.append(loss.item())
+    return np.mean(val_loss_per_epoch), z_mean, z_log_var, reconstruction
+
+
+def train_cVAE(model, train_loader, test_loader, beta, criteration = loss_vae, learning_rate=1e-3, optimizer = None, epochs=25, batch_size=128,  plot_history=True, latent_dim=16,
+                  save_model=True, saving_path='./cVAE_models/'):
+
+    optimizer_ = torch.optim.Adam(model.parameters(), lr=1e-3)
+    def __plot_history(history):
+        plt.figure(figsize=(10, 5))
+        plt.plot(history['loss'], label='Train Loss')
+        plt.plot(history['val_loss'], label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        model_name = "cVAE_Name_zdim_" + str(latent_dim)+"_epochs_"+str(epochs)
+        plt.savefig(saving_path+ 'loss_'+model_name+'.png')
+        plt.show()
+
+    # Lists to store loss history for plotting
+    train_loss_history = []
+    val_loss_history = []
+
+    # Training loop
+    for epoch in range(epochs):
+        
+        # Train
+        train_loss, z_mean, z_log_var, reconstruction = train_epoch(model, beta=beta, criterion=criteration, optimizer=optimizer_, data_loader=train_loader)
+        train_loss_history.append(train_loss)                                            
+        # Validation
+        eval_loss, z_mean, z_log_var, reconstruction = eval_epoch(model, beta=beta, criterion=criteration, optimizer=optimizer_, data_loader=test_loader)
+        val_loss_history.append(eval_loss) 
+        print(f"Epoch [{epoch+1}/{epochs}] - Train Loss: {train_loss :.4f} - Val Loss: {eval_loss:.4f}")
+
+    if plot_history:
+        history = {'loss': train_loss_history, 'val_loss': val_loss_history}
+        __plot_history(history)
+
+    if save_model:
+        model_name = "cVAE_Name_zdim_" + str(latent_dim)+"_beta_"+str(beta)+"_epochs_"+str(epochs)
+        torch.save(model.state_dict(), saving_path+model_name+".pth")
+
+=======
 #!/usr/bin/env python
 # coding: utf-8
 # @author Souheib Ben Mabrouk
@@ -84,4 +173,93 @@ def train_cVAE(model, train_loader, test_loader, beta, criteration = loss_vae, l
         model_name = "cVAE_Name_zdim_" + str(latent_dim)+"_beta_"+str(beta)+"_epochs_"+str(epochs)
         torch.save(model.state_dict(), saving_path+model_name+".pth")
 
+>>>>>>> 61b1a12eda44206a83eec5925a04cbe96d1a7ec3
+=======
+#!/usr/bin/env python
+# coding: utf-8
+# @author Souheib Ben Mabrouk
+
+
+import numpy as np 
+import torch
+import matplotlib.pyplot as plt
+from cVAE_architecture2 import loss_vae
+
+device='cuda:1' if torch.cuda.is_available() else 'cpu'
+clip_value = 1.0 
+batch_size = 128
+
+def clip_grads(model, clip_value):
+    for param in model.parameters():
+        if param.grad is not None:
+            param.grad.data = torch.clamp(param.grad.data, -clip_value, clip_value)
+
+def train_epoch(model, beta, criterion, optimizer, data_loader):
+    train_loss_per_epoch = []
+    model.train()
+    for x_batch, y in data_loader:
+        y = y.to(device)
+        x_batch = x_batch.to(device)    
+        z_mean, z_log_var, reconstruction = model(x_batch, y)
+        loss = criterion(beta, x_batch.to(device).float(), z_mean, z_log_var, reconstruction) 
+        optimizer.zero_grad()
+        loss.backward()
+        #clip_grads(model, 1.0)
+        optimizer.step()
+        train_loss_per_epoch.append(loss.item())
+    return np.mean(train_loss_per_epoch), z_mean, z_log_var, reconstruction
+
+
+def eval_epoch(model, beta, criterion, optimizer, data_loader):
+    val_loss_per_epoch = []
+    model.eval()
+    with torch.no_grad():
+        for x_val, y in data_loader:
+            y = y.to(device)
+            x_val = x_val.to(device)
+            z_mean, z_log_var, reconstruction = model(x_val, y)
+            loss = criterion(beta, x_val.to(device).float(), z_mean, z_log_var, reconstruction) 
+            val_loss_per_epoch.append(loss.item())
+    return np.mean(val_loss_per_epoch), z_mean, z_log_var, reconstruction
+
+
+def train_cVAE(model, train_loader, test_loader, beta, criteration = loss_vae, learning_rate=1e-3, optimizer = None, epochs=25, batch_size=128,  plot_history=True, latent_dim=16,
+                  save_model=True, saving_path='./cVAE_models/'):
+
+    optimizer_ = torch.optim.Adam(model.parameters(), lr=1e-3)
+    def __plot_history(history):
+        plt.figure(figsize=(10, 5))
+        plt.plot(history['loss'], label='Train Loss')
+        plt.plot(history['val_loss'], label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        model_name = "cVAE_Name_zdim_" + str(latent_dim)+"_epochs_"+str(epochs)
+        plt.savefig(saving_path+ 'loss_'+model_name+'.png')
+        plt.show()
+
+    # Lists to store loss history for plotting
+    train_loss_history = []
+    val_loss_history = []
+
+    # Training loop
+    for epoch in range(epochs):
+        
+        # Train
+        train_loss, z_mean, z_log_var, reconstruction = train_epoch(model, beta=beta, criterion=criteration, optimizer=optimizer_, data_loader=train_loader)
+        train_loss_history.append(train_loss)                                            
+        # Validation
+        eval_loss, z_mean, z_log_var, reconstruction = eval_epoch(model, beta=beta, criterion=criteration, optimizer=optimizer_, data_loader=test_loader)
+        val_loss_history.append(eval_loss) 
+        print(f"Epoch [{epoch+1}/{epochs}] - Train Loss: {train_loss :.4f} - Val Loss: {eval_loss:.4f}")
+
+    if plot_history:
+        history = {'loss': train_loss_history, 'val_loss': val_loss_history}
+        __plot_history(history)
+
+    if save_model:
+        model_name = "cVAE_Name_zdim_" + str(latent_dim)+"_beta_"+str(beta)+"_epochs_"+str(epochs)
+        torch.save(model.state_dict(), saving_path+model_name+".pth")
+
+>>>>>>> 61b1a12eda44206a83eec5925a04cbe96d1a7ec3
     return model
